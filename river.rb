c1efed674322a3a16910f2834f57dfc14a977638
YingqiTao <yingqitao2000@gmail.com>
2018-08-10 17:43:51 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,1 +1,43 @@
 # river.rb
+class River 
+  def initialize(name, length, countries, discharge)
+    @name = name
+    @length = length
+    @countries = countries
+    @discharge = discharge
+  end
+  
+  attr_accessor :length, :countries, :discharge
+  
+  # def length
+  #   @length
+  # end
+  
+  # def countries
+  #   @countries
+  # end
+  
+  # def discharge
+  #   @discharge
+  # end
+  
+  def flood
+    @discharge = @discharge * 1.3
+  end
+  
+  def dry_up
+    @discharge = @discharge * 0.5
+  end
+end
+
+nile = River.new("The Nile", 4258, )
+mississippi = River.new("The Mississippi")
+amazon = River.new("The Amazon")
+seine = River.new("The Seine")
+yangtze = River.new("The Yangtze")
+euphrates = River.new("The Euphrates")
+
+amazon.flood
+yangtze.flood
+
+euphrates.dry_up
