2f8fe5a7b2b23ed271245f315838be855381c78e
YingqiTao <yingqitao2000@gmail.com>
2018-08-10 17:55:14 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -31,7 +31,7 @@ class River
 end
 
 nile = River.new("The Nile", 4160, "Tanzania, Burundi, Rwanda, the Democratic Republic of the Congo, Kenya, Uganda, South Sudan, Ethiopia, Sudan", 99940)
-mississippi = River.new("The Mississippi", 3710, 593000)
+mississippi = River.new("The Mississippi", 3710, "United States", 593000)
 amazon = River.new("The Amazon", 4000, 7381000)
 seine = River.new("The Seine")
 yangtze = River.new("The Yangtze", 3964, 1605000)
